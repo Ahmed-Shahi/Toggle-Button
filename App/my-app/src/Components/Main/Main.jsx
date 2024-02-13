4acac90c411a1a30beb992da060a5bedcb73bb28
Ahmed-Shahi <ahmedshahi038@gmail.com>
2024-02-14 02:07:42 +0500

    ok

--- App/my-app/src/Components/Main/Main.jsx
+++ App/my-app/src/Components/Main/Main.jsx
@@ -4,6 +4,20 @@ export default function Main() {
     return (
         <div>
             {/* 1 */}
+            <div>
+                <div className='logodiv'> 
+                <img src='' alt="" />
+                </div>
+                <div style={{display:'flex',}}>
+                    <button type="button" class="btn btn-outline-light" style={{marginRight:'20px'}}>
+                           Sign IN 
+                    </button>
+                    <button type="button" class="btn btn-info">
+                            Sign UP
+                    </button>
+                </div>
+            </div>
+            {/* 1 */}
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60vh' }}>
                 <div style={{ width: '50%' }}>
                     <p style={{ color: 'white', fontWeight: 'bold' }}> <span style={{ fontSize: '45px', wordSpacing: '50px', textTransform: 'uppercase' }}>Lorem ipsum dolor sit</span><br />
@@ -38,15 +52,16 @@ export default function Main() {
             {/* 5 */}
             <div >
                 <div class="card-group" >
-                    <div class="card" style={{backgroundColor:'rgba(0,0,0,0)',}}>
+                    <div class="card" style={{ backgroundColor: 'rgba(0,0,0,0)', }}>
                         <img src="" class="card-img-top" alt="..." />
+
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
-                    <div class="card" style={{backgroundColor:'rgba(0,0,0,0'}}>
+                    <div class="card" style={{ backgroundColor: 'rgba(0,0,0,0' }}>
                         <img src="..." class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -54,7 +69,7 @@ export default function Main() {
                             <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
-                    <div class="card" style={{backgroundColor:'rgba(0,0,0,0'}}>
+                    <div class="card" style={{ backgroundColor: 'rgba(0,0,0,0' }}>
                         <img src="..." class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
